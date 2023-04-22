d2ac9bc1a054900b67a7dd7372e03cd1ae5076e6
su-pernova <sumikim323@naver.com>
2023-04-22 21:46:26 +0900

    :sparkles: REST API GET mapping 연습

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -1,9 +1,11 @@
 package com.example.mvc.controller.get
 
+import com.example.mvc.model.http.UserRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController // REST API Controller로 동작함을 명시
@@ -39,15 +41,33 @@ class GetApiController {
 
     // @PathVariable(value = "name") 식으로 매칭이 가능하다.
     // PathVariable에서 사용한 변수명을 함수 내부에서도 사용하고 싶을 때 활용 가능
-    @GetMapping("/get-mapping/path-variable2/{name}")
-    fun pathVariable3(@PathVariable(value = "name") inputName: String): String {
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+        @RequestParam(value = "name") inputName: String,
+        @RequestParam(value = "age") inputAge: Int
+    ): String {
         val name = "steve"
-        return "$name $inputName"
+        return "$name $inputName $inputAge"
     }
 
-    @GetMapping("/get")
-    fun queryParam() {
-
+    // queryParameter를 객체로 Mapping 하는 방법
+    // 스프링에서 @RestController 를 사용하는 경우
+    // Return Type이 Object인 경우 결과는 JSON format에 매핑된다.
+    // 단 Kotlin 에선 변수명에 하이픈(-)이 들어갈 수 없으므로, 설계시 주의 필요
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
     }
+
+    // Map 형식으로 받는 방법
+    // Map : Key-value 쌍의 데이터 타입 (파이썬 딕셔너리와 동일)
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String,Any>): Any? {
+        println(map)
+        val phoneNumber = map.get("phone-number")
+        return phoneNumber
+    }
+
 
 }

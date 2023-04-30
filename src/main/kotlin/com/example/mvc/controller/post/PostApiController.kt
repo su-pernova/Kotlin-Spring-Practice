8fa1cf81b4044bd7379d6b65c8a98621884ce95b
김수미 서비스개발팀 <sumikim@sk.com>
2023-04-30 17:58:53 +0900

    ✨ REST API POST mapping 연습

---
+++ src/main/kotlin/com/example/mvc/controller/post/PostApiController.kt
@@ -0,0 +1,36 @@
+package com.example.mvc.controller.post
+
+import com.example.mvc.model.http.UserRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PostApiController {
+
+    @PostMapping("/post-mapping")
+    fun postMapping(): String {
+        return "post-mapping"
+    }
+
+    // @PostMapping 어노테이션 대신 @RequestMapping 어노테이션을 사용하는 경우
+    // GET 메소드에서도 'request-mapping' UTI 주소를 사용했지만, 메소드 형식이 POST 로 다르기 때문에 충돌이 발생하지 않는다.
+    @RequestMapping(method = [RequestMethod.POST], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    // Object Mapper 라이브러리를 사용해서 요청을 받아보자.
+    // Object Mapper : json -> object 로, object -> json 으로 바꿔주는 라이브러리
+    // Request Body 로 Request 를 받고, 받은 내용을 그대로 반환하는 함수
+    @PostMapping("/post-mapping/object")
+    fun postMappingObject(@RequestBody userRequest: UserRequest): UserRequest { // Request : json -> object
+        println(userRequest)
+        return userRequest // Response : object -> json
+    }
+
+
+}
--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,11 +1,20 @@
 package com.example.mvc.model.http
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 
+
+// 필드의 개수가 20개가 넘어가는 경우, 각 필드에 일일이 @JsonProperty 를 지정해 주는 것은 매우 번거로울 수 있다.
+// 이 때 @JsonNaming 어노테이션을 사용하면 클래스의 모든 필드에 대한 naming strategy 를 지정해줄 수 있다.
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class UserRequest(
     var name: String? = null,
     var age: Int? = null,
     var email: String? = null,
-    var address: String? = null
-) {
+    var address: String? = null,
 
-}
+    // @JsonProperty 어노테이션을 사용하면 코드 내에서는 phoneNumber 라는 이름을 사용하지만 (CamelCase)
+    // Json 요청을 받고 응답을 보낼 때에는 필드 값을 phone_number 라는 이름으로 표시할 수 있다.
+    // @JsonProperty("phone_number")
+    var phoneNumber: String? = null
+)

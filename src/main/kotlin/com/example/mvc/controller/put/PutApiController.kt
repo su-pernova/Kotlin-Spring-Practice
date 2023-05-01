8e6703da738320e75815755ff1cd28101f4690fd
김수미 서비스개발팀 <sumikim@sk.com>
2023-05-01 13:20:11 +0900

    :sparkles: 유효성 검증 Annotation 사용 연습

--- src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -3,11 +3,15 @@ package com.example.mvc.controller.put
 import com.example.mvc.model.http.Result
 import com.example.mvc.model.http.UserRequest
 import com.example.mvc.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 // PUT 은 DB 에 이미 해당 내용이 있으면 수정을, 없으면 생성을 처리한다.
 @RestController
@@ -24,16 +28,32 @@ class PutApiController {
         return "request-mapping-put-method"
     }
 
+    // @RequestBody 앞에 @Valid 어노테이션을 붙여 줘야 입력 클래스에 대한 유효성 검증을 실시한다.
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject1(@RequestBody userRequest: UserRequest): UserRequest {
+    fun putMappingObject1(@Valid @RequestBody userRequest: UserRequest): UserRequest {
         return userRequest
     }
 
+    // BindingResult 를 이용해 발상해는 에러를 잡아 ResponseEntity 로 내보낼 수 있다.
     @PutMapping(path = ["/put-mapping/object/detail"])
-    fun putMappingObject2(@RequestBody userRequest: UserRequest): UserResponse {
+    fun putMappingObject2(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
+
+        if(bindingResult.hasErrors()) {
+            // 500 error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field + " : " + message + "\n")
+            }
+            return ResponseEntity.badRequest().body(msg.toString())
+        }
+
+        return ResponseEntity.ok("")
+
         // Response
         // apply 패턴 사용
-        return UserResponse().apply {
+        /* return UserResponse().apply {
             // 1. result
             this.result = Result().apply {
                 this.resultCode = "OK"
@@ -61,7 +81,7 @@ class PutApiController {
                 this.phoneNumber = "010-1234-1234"
             })
             this.user = userList
-        }
+        } */
     }
 
 
--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -2,19 +2,46 @@ package com.example.mvc.model.http
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.*
 
 
-// 필드의 개수가 20개가 넘어가는 경우, 각 필드에 일일이 @JsonProperty 를 지정해 주는 것은 매우 번거로울 수 있다.
+// 필드의 개수가 20개가 넘어가는 경우, 각 필드에 일일이 @JsonProperty 를 지정해 주는 것은 번거로울 수 있다.
 // 이 때 @JsonNaming 어노테이션을 사용하면 클래스의 모든 필드에 대한 naming strategy 를 지정해줄 수 있다.
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class UserRequest (
+    // Kotlin 에서는 Java 와 달리 validation 을 작성할 때 filed 키워드를 사용한다.
+    @field:NotEmpty
+    @field:Size(min = 2, max = 8)
     var name: String? = null,
+
+    @field:PositiveOrZero // 0 이상
     var age: Int? = null,
+
+    @field:Email
     var email: String? = null,
+
+    @field:NotBlank
     var address: String? = null,
 
     // @JsonProperty 어노테이션을 사용하면 코드 내에서는 phoneNumber 라는 이름을 사용하지만 (CamelCase)
     // Json 요청을 받고 응답을 보낼 때에는 필드 값을 phone_number 라는 이름으로 표시할 수 있다.
     // @JsonProperty("phone_number")
-    var phoneNumber: String? = null
-)
+    @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$") // 휴대폰 번호 형식을 검증하는 정규식
+    var phoneNumber: String? = null,
+
+    var createdAt: String? = null // yyyy-mm-dd hh:mm:ss (2020-10-02 12:00:00)
+){
+    // Validation 어노테이션으로 지원되지 않는 부분을 검증하고자 할 때는 아래처럼 직접 함수를 작성할 수도 있다.
+    @AssertTrue(message = "생성일자의 패턴은 yyyy-mm-dd hh:mm:ss 여야 합니다.")
+    private fun isValidCreatedAt(): Boolean { // 정상 : true, 비정상 : false 반환
+        try {
+            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            return true
+        } catch (e: Exception){
+            return false
+        }
+    }
+
+}
